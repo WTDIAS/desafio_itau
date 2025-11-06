8ff1054a946c3ee1d43cd7d5ad66b5532a1bb55d
Waldir <waldirtiagodias@gmail.com>
2025-11-06 14:45:09 -0300

    Extras 3 - Implementação de log. Correção de retorno de estatísticas zero após filtro. Correção na verificação de time zone.

--- src/main/java/br/com/gigalike/desafioItau/service/TransacaoService.java
+++ src/main/java/br/com/gigalike/desafioItau/service/TransacaoService.java
@@ -3,8 +3,11 @@ import br.com.gigalike.desafioItau.dto.EstatisticaDto;
 import br.com.gigalike.desafioItau.dto.TransacaoDto;
 import br.com.gigalike.desafioItau.exception.DesafioItauException422;
 import br.com.gigalike.desafioItau.utils.FormatadorNumerico;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
@@ -14,41 +17,53 @@ import java.util.stream.Collectors;
 public class TransacaoService {
 
     private final List<TransacaoDto> transacoes = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger(TransacaoService.class);
+    private OffsetDateTime agora;
 
     public void adicionarTransacao(TransacaoDto transacaoDto) {
+        agora = OffsetDateTime.now(ZoneOffset.of("-03:00"));
         if (
                 transacaoDto.valor() == null ||
                 transacaoDto.dataHora() == null ||
                 transacaoDto.valor() < 0 ||
-                transacaoDto.dataHora().isAfter(OffsetDateTime.now())
+                !transacaoDto.dataHora().isBefore(agora)
         )
         {
+            log.warn("Não foi possível salvar a transação: {}",transacaoDto);
             throw new DesafioItauException422("");
         }
         transacoes.add(transacaoDto);
-        System.out.println(transacoes);
+        log.info("Transação adicionada: {}",transacaoDto);
     }
 
     public void limparTransacoes(){
         transacoes.clear();
-        System.out.println(transacoes);
+        log.info("Transações excluidas: {}",transacoes);
     }
 
     public EstatisticaDto calcularEstatistica(int segundos){
+        EstatisticaDto estatisticaDtoZerado = new EstatisticaDto(0,0,0,0,0);
         if (transacoes.isEmpty()){
-            return new EstatisticaDto(0,0,0,0,0);
+            log.info("Calcular estatística foi chamado para lista vazia.");
+            return estatisticaDtoZerado;
         }
 
-        OffsetDateTime agora = OffsetDateTime.now();
+        agora = OffsetDateTime.now(ZoneOffset.of("-03:00"));
         OffsetDateTime intervaloDeCalculo = agora.minusSeconds(segundos);
 
-        DoubleSummaryStatistics dss = transacoes.stream()
+        List<TransacaoDto> transacoesFiltradas = transacoes.stream()
                 .filter(transacao -> !transacao.dataHora().isBefore(intervaloDeCalculo))
+                .toList();
+
+        if (transacoesFiltradas.isEmpty()){
+            log.warn("Não encontrado transações para o período de: {}",segundos);
+            return estatisticaDtoZerado;
+        }
+
+        DoubleSummaryStatistics dss = transacoesFiltradas.stream()
                 .collect(Collectors.summarizingDouble(TransacaoDto::valor));
 
-        System.out.println("AGORA: "+agora);
-        System.out.println(dss);
-
+        log.info("Retorno do cálculo de estatística para {} segundos: {}. ",segundos,transacoesFiltradas);
         return new EstatisticaDto(
                 dss.getCount(),
                 FormatadorNumerico.formatarParaDuasCasasDecimais(dss.getSum()),
--- src/main/java/br/com/gigalike/desafioItau/utils/FormatadorNumerico.java
+++ src/main/java/br/com/gigalike/desafioItau/utils/FormatadorNumerico.java
@@ -1,9 +1,19 @@
 package br.com.gigalike.desafioItau.utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Locale;
 
 public class FormatadorNumerico {
 
+    public static final Logger log = LoggerFactory.getLogger(FormatadorNumerico.class);
+
     public static double formatarParaDuasCasasDecimais(Double valor){
-            return Double.parseDouble(String.format(Locale.US,"%.2f",valor));
+        double retorno = 0.0;
+        try {
+            retorno = Double.parseDouble(String.format(Locale.US, "%.2f", valor));
+        }catch (Exception e){
+            log.error("Erro na formatação de casas decimais para o valor: {}",valor,e);
+        }
+        return retorno;
     }
 }
