d2c9fbfdad0dc075d724d7f2ad1fde660abc96c2
Waldir <waldirtiagodias@gmail.com>
2025-11-09 10:30:07 -0300

    Conclusão documentação API

--- src/main/java/br/com/gigalike/desafioItau/controller/TransacaoController.java
+++ src/main/java/br/com/gigalike/desafioItau/controller/TransacaoController.java
@@ -2,6 +2,8 @@ package br.com.gigalike.desafioItau.controller;
 import br.com.gigalike.desafioItau.dto.EstatisticaDto;
 import br.com.gigalike.desafioItau.dto.TransacaoDto;
 import br.com.gigalike.desafioItau.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,23 +11,27 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@Tag(name = "Transação", description = "Endpoints de gestão de transações.")
 public class TransacaoController {
 
     @Autowired
     private TransacaoService transacaoService;
 
+    @Operation(summary = "Cadastro de transação", description = "Faz a inclusão da transação na memória.")
     @PostMapping("/transacao")
     public ResponseEntity<Void> cadastrarTransacao(@RequestBody TransacaoDto transacaoDto){
         transacaoService.adicionarTransacao(transacaoDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Limpa todas as transações", description = "Realiza a exclusão de todas transações incluidas na memória.")
     @DeleteMapping("/transacao")
     public ResponseEntity<Void> limparTransacoes(){
         transacaoService.limparTransacoes();
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Exibe as estatísticas", description = "Retorna a estatística das transações contendo: contagem, soma, média, minimo e máximo ")
     @GetMapping("/estatistica")
     public ResponseEntity<EstatisticaDto> calcularEstatistica(@RequestParam(name = "segundos", defaultValue = "60") int segundos){
         EstatisticaDto estatisticaDto = transacaoService.calcularEstatistica(segundos);
--- src/test/java/br/com/gigalike/desafioItau/controller/TransacaoControllerTest.java
+++ src/test/java/br/com/gigalike/desafioItau/controller/TransacaoControllerTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @WebMvcTest(TransacaoController.class)
 class TransacaoControllerTest {
@@ -22,7 +22,7 @@ class TransacaoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private TransacaoService transacaoService;
 
     @Test
