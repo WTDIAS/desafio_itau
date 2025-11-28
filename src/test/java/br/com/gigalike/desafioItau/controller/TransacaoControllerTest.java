856c27851066a38fd511c1f0d031cfce23a5b06f
Waldir <waldirtiagodias@gmail.com>
2025-11-28 17:58:09 -0300

    correção nos testes, filtros de segurança desativados para testes

--- src/test/java/br/com/gigalike/desafioItau/controller/TransacaoControllerTest.java
+++ src/test/java/br/com/gigalike/desafioItau/controller/TransacaoControllerTest.java
@@ -8,22 +8,21 @@ import br.com.gigalike.desafioItau.service.TransacaoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.springframework.boot.test.mock.mockito.MockBean;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @WebMvcTest(TransacaoController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class TransacaoControllerTest {
 
     @Autowired
